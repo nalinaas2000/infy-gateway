78b3568207940df20458f038afd859c5ca5ccca1
nalinaas2000 <nalina.as2000@gmail.com>
2022-07-22 17:00:58 +0530

    first ever commit

--- src/com/infy/message/MyMessage.java
+++ src/com/infy/message/MyMessage.java
@@ -4,7 +4,7 @@ public class MyMessage {
 	public static void main (String args[]) {
 		
 	
-System.out.println("Nalina 1266009 Infosys : Hello Everyone");
+System.out.println("Nalina 1266009 Infosys : Hello Nalina");
 	}
 
 }
